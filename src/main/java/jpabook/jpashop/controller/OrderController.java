c4e6b6b910051d5ca3337b331a13a2bf3cad3bb1
ziwookim <ziwookiim@gmail.com>
2022-09-08 00:37:31 +0900

    developed ordering items.

---
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        // ** 가급적 식별자만 파라미터로 넘겨서 @Transaction 어노테이션이 존재하는 (영속 Context가 존재하는) 영역에서 엔티티를 처리하도록 한다.
+        // 가독성 향상, 테스트 작업 원활
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
